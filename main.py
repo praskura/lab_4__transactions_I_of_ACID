c4f675797eb5d9c2c7bb9ac9e93ec441aae8307f
Grigorii Praskura <gregorypraskura@hotmail.com>
2024-06-26 19:50:07 +0300

    Add operations in transactions

--- main.py
+++ main.py
@@ -20,16 +20,66 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
+@app.get("/1")
 async def main():
     conn_1 = await asyncpg.connect(dsn=settings.database_dsn)
     logging.info('DB connection 1 has been established')
 
+    async with conn_1.transaction():
+        await conn_1.execute("INSERT INTO values (number) VALUES (2)")
+        await conn_1.execute(
+            """UPDATE values
+            SET number = 3
+            WHERE id = 1;"""
+        )
+        await conn_1.execute("INSERT INTO values (number) VALUES (3)")
+        await conn_1.execute(
+            """UPDATE values
+            SET number = 3
+            WHERE id = 2;"""
+        )
+        await asyncio.sleep(5)
+        await conn_1.execute("DELETE FROM values WHERE id = 1")
+        await conn_1.execute("INSERT INTO values (number) VALUES (4)")
+        await conn_1.execute(
+            """UPDATE values
+            SET number = 5
+            WHERE id = 2;"""
+        )
+        
+    await conn_1.close()
+    logging.info('DB connection 1 closed')
+
+    return {
+        "status": "ok"
+    }
+
+
+@app.get("/2")
+async def main():
     conn_2 = await asyncpg.connect(dsn=settings.database_dsn)
     logging.info('DB connection 2 has been established')
 
-    await conn_1.close()
-    logging.info('DB connection 1 closed')
+    async with conn_2.transaction():
+        await conn_2.execute("INSERT INTO values (number) VALUES (2)")
+        await conn_2.execute(
+            """UPDATE values
+            SET number = 3
+            WHERE id = 2;"""
+        )
+        await conn_2.execute("INSERT INTO values (number) VALUES (5)")
+        await conn_2.execute(
+            """UPDATE values
+            SET number = 3
+            WHERE id = 1;"""
+        )
+        await conn_2.execute("DELETE FROM values WHERE id = 1")
+        await conn_2.execute("INSERT INTO values (number) VALUES (4)")
+        await conn_2.execute(
+            """UPDATE values
+            SET number = 5
+            WHERE id = 2;"""
+        )
 
     await conn_2.close()
     logging.info('DB connection 2 closed')
